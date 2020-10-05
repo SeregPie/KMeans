c74e64a8f349b26c240a7535209d9c9502f8cb02
Sergej Sintschilin <seregpie@gmail.com>
2020-10-05 13:32:32 +0200

    replace buble with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,21 @@
 import {terser} from 'rollup-plugin-terser';
-import buble from '@rollup/plugin-buble';
+import babel from '@rollup/plugin-babel';
 
 import {main} from './package.json';
 
 let globals = {
-	'just-my-luck': 'JustMyLuck',
 	'@seregpie/vector-math': 'VectorMath',
+	'just-my-luck': 'JustMyLuck',
 };
 
 export default {
 	external: Object.keys(globals),
 	input: 'src/index.js',
 	plugins: [
-		buble(),
+		babel({
+			babelHelpers: 'bundled',
+			presets: ['@babel/preset-env'],
+		}),
 		terser(),
 	],
 	output: {
